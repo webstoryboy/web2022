4b8e7f964a81f6115b085d7a5f0e6867359ea3c5
webstoryboy <webstupids@gmail.com>
2022-12-27 07:52:48 +0900

    2022.12.27

--- webstoryboy/three/three_webpack/src/app.js
+++
@@ -1,6 +0,0 @@
-import { view } from "./main.js";
-import "./app.css";
-
-window.addEventListener("DOMContentLoaded", () => {
-  view();
-});
--- webstoryboy/three/three_webpack/src/main.js
+++
@@ -1,8 +0,0 @@
-import Img from "./webpack.svg";
-
-export function view() {
-  document.querySelector("#app").innerHTML = `
-    <h1>hello world</h1>
-    <img src = "${Img}" alt="img" />
-  `;
-}
